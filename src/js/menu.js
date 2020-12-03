96c04750d4434c1d5d50afa0e9ccaf83c7a3b84f
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-12-04 00:30:04 +0200

    Added comments.

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,5 +2,8 @@ import { menuListRef } from './refs.js';
 import menu from '../menu.json';
 import menuTemplate from '../templates/menu-item.hbs';
 
+// Создание разметки для всего массива объектов меню с помощью шаблонизатора
 const menuMarkup = menuTemplate(menu);
+
+// Добавление результата функции шаблонизатора в DOM
 menuListRef.insertAdjacentHTML('afterbegin', menuMarkup);
